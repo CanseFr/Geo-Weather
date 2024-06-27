82c7590e292e07eea893fb87504a51e4146b5f40
CanseFr <jucansell@gmail.com>
2024-06-27 14:29:26 +0200

    :lipstick: Upgrade user space setting form

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -12,8 +12,12 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPageState extends State<AccountPage> {
+
   final _usernameController = TextEditingController();
   final _websiteController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _firstnameController = TextEditingController();
+  final _lastnameController = TextEditingController();
 
   String? _avatarUrl;
   var _loading = true;
@@ -30,6 +34,9 @@ class _AccountPageState extends State<AccountPage> {
       await supabase.from('profiles').select().eq('id', userId).single();
       _usernameController.text = (data['user'] ?? '') as String;
       _websiteController.text = (data['website'] ?? '') as String;
+      _phoneController.text = (data['phone'] ?? '') as String;
+      _firstnameController.text = (data['first_name'] ?? '') as String;
+      _lastnameController.text = (data['last_name'] ?? '') as String;
       _avatarUrl = (data['avatar_url'] ?? '') as String;
     } on PostgrestException catch (error) {
       if (mounted) context.showSnackBar(error.message, isError: true);
@@ -53,11 +60,17 @@ class _AccountPageState extends State<AccountPage> {
     });
     final userName = _usernameController.text.trim();
     final website = _websiteController.text.trim();
+    final phone = _phoneController.text.trim();
+    final firstname = _firstnameController.text.trim();
+    final lastname = _lastnameController.text.trim();
     final user = supabase.auth.currentUser;
     final updates = {
       'id': user!.id,
       'user': userName,
       'website': website,
+      'phone': phone,
+      'first_name': firstname,
+      'last_name': lastname,
       'updated_at': DateTime.now().toIso8601String(),
     };
     try {
@@ -106,6 +119,9 @@ class _AccountPageState extends State<AccountPage> {
   void dispose() {
     _usernameController.dispose();
     _websiteController.dispose();
+    _phoneController.dispose();
+    _firstnameController.dispose();
+    _lastnameController.dispose();
     super.dispose();
   }
 
@@ -116,22 +132,100 @@ class _AccountPageState extends State<AccountPage> {
       body: ListView(
         padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 12),
         children: [
-          TextFormField(
-            controller: _usernameController,
-            decoration: const InputDecoration(labelText: 'User Name'),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              // TITLE
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Center(
+                  child: Text(
+                    "Modification de vos informations personnelles",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+
+              // USER NAME
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 200),
+                child: TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'User Name',
+                    contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 18),
+
+              // BIO
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 200),
+                child: TextFormField(
+                  controller: _websiteController,
+                  decoration: InputDecoration(
+                    labelText: 'Bio',
+                    contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 18),
+
+              // PHONE
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 200),
+                child: TextFormField(
+                  controller: _phoneController,
+                  decoration: InputDecoration(
+                    labelText: 'Phone',
+                    contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 18),
+
+              // Firstname
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 200),
+                child: TextFormField(
+                  controller: _firstnameController,
+                  decoration: InputDecoration(
+                    labelText: 'Firstname',
+                    contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 18),
+
+              // Lastname
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 200),
+                child: TextFormField(
+                  controller: _lastnameController,
+                  decoration: InputDecoration(
+                    labelText: 'Lastname',
+                    contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 18),
+
+
+              ElevatedButton(
+                onPressed: _loading ? null : _updateProfile,
+                child: Text(_loading ? 'Saving...' : 'Update'),
+              ),
+              const SizedBox(height: 18),
+              TextButton(onPressed: _signOut, child: const Text('Sign Out')),
+            ],
           ),
-          const SizedBox(height: 18),
-          TextFormField(
-            controller: _websiteController,
-            decoration: const InputDecoration(labelText: 'Website'),
-          ),
-          const SizedBox(height: 18),
-          ElevatedButton(
-            onPressed: _loading ? null : _updateProfile,
-            child: Text(_loading ? 'Saving...' : 'Update'),
-          ),
-          const SizedBox(height: 18),
-          TextButton(onPressed: _signOut, child: const Text('Sign Out')),
+
+
+
         ],
       ),
     );
